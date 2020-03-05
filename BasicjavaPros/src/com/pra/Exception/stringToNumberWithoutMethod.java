0a72bdb527e10172fa27aa0475fd34134a9cd050
debasisd <debasisd@DEBASISD-IN.in.oracle.com>
2020-03-05 12:34:56 +0530

    ss

--- BasicjavaPros/src/com/pra/Exception/stringToNumberWithoutMethod.java
+++ BasicjavaPros/src/com/pra/Exception/stringToNumberWithoutMethod.java
@@ -4,10 +4,15 @@ public class stringToNumberWithoutMethod {
 	public static int convert_String_To_Number(String numStr) {
 		char ch[] = numStr.toCharArray();
 		int sum = 0;
+		
 		//get ascii value for zero
 		int zeroAscii = (int)'0';
+		System.out.println("zeroAscii----"+zeroAscii);
+		
 		for (char c:ch) {
+			System.out.println("char of each string-"+c);
 			int tmpAscii = (int)c;
+			
 			sum = (sum*10)+(tmpAscii-zeroAscii);
 		}
 		return sum;
